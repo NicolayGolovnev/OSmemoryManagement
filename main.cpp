b4d2d06e42e3a23fd91565af11233571e972637f
Nicolay Golovnev <kolya.golovnev@mail.ru>
2020-05-08 12:58:04 +0700

    Start work on project

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+
+using namespace std;
+
+class Process{
+public:
+    
+}
+
+int MEMORY[10000000]; // Наша оперативная память = 1.000.000 байт
+vector <Process> query; // наша приоритетная очередь приложений
+
+int main(){
+
+}
