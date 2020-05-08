e8ac379919af6645b9cdff8fdc36ff390731ec61
Nicolay Golovnev <kolya.golovnev@mail.ru>
2020-05-08 13:01:33 +0700

    Write a parametrs of class

--- main.cpp
+++ main.cpp
@@ -5,8 +5,13 @@
 using namespace std;
 
 class Process{
-public:
+private:
+    int timeLife = -1; // время жизни процесса
+    vector <pair<int, int> > inMemory; // места расположения процесса в памяти
+    int priority = -1; // приоритет процесса
+    int memory = 0; // сколько занимает места процесс в оперативной памяти
     
+
 }
 
 int MEMORY[10000000]; // Наша оперативная память = 1.000.000 байт
