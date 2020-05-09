537a96accc188a77e1ee1e6c180e114246e2b340
Nicolay Golovnev <kolya.golovnev@mail.ru>
2020-05-09 22:41:46 +0700

    Most comments created on Russian

--- main.cpp
+++ main.cpp
@@ -18,7 +18,7 @@ private:
     int timeLife; // время жизни процесса
     clock_t lastAccess; // время последнего обращения к процессу
     
-    int swapping = -1; //указывает, находится ли процесс в файле своппинга или нет
+    int swapping = -1; // указывает, находится ли процесс в файле своппинга или нет
     pair<int, int> inMemory; // место расположения процесса в памяти
     
 public:
@@ -28,6 +28,7 @@ public:
         this->memory = mem;
         this->priority = pr;
         this->timeLife = time;
+        // последнее время использование процесса, для правильного отображения остатка времени
         this->lastAccess = clock() / CLOCKS_PER_SEC;
     };
 
@@ -70,7 +71,7 @@ public:
     
 };
 
-//перегрузка операторов для сортировки вектора-массива
+// перегрузка операторов для сортировки вектора-массива
 bool operator>(Process a, Process b){
     return (a.getPriority() > b.getPriority() ? true : false);
 }
@@ -78,13 +79,16 @@ bool operator<(Process a, Process b){
     return (a.getPriority() < b.getPriority() ? true : false);
 }
 
-int MEMORY[CAPACITY]; // Наша оперативная память = 1.000.000 байт, хранится в 1.000 блоках каждый по 1.000 байт
+int MEMORY[CAPACITY]; // наша оперативная память
 vector <Process> query; // наша приоритетная очередь приложений
 vector <Process> procInMemory; // процессы, которые хранятся в памяти и работают в данный момент
 
-//если памяти недостаточно, то подается текущий процесс в файл своппинга и ожидает, когда освободиться память достаточно для того,
-//чтобы выгрузить ее в память для дальнейшей работы с ним
+// если памяти недостаточно, то подается текущий процесс в файл своппинга и ожидает, когда освободиться память достаточно для того,
+// чтобы выгрузить ее в память для дальнейшей работы с ним
 
+// функция создания процесса
+// описывание всех нужных параметром процесса
+// при указании памяти больше доступной, процесс будет постоянно хранится в своппинг-файле
 bool createProcess(){
     system("cls");
     cout << "Enter a name of process: ";
@@ -107,6 +111,7 @@ bool createProcess(){
     return 1;
 }
 
+// обновление времени жизни процесса и чистка памяти
 void updMemory(clock_t time){
     vector <int> forDelete;
 
@@ -115,6 +120,7 @@ void updMemory(clock_t time){
             query[i].setTimeLife(query[i].getTimeLife() - (time - query[i].getLastAccess()));
             query[i].setLastAccess(time);
         }
+        // очистка памяти
         if (query[i].getTimeLife() <= 0){
             for (int refresh = query[i].getInMemory().first; refresh <= query[i].getInMemory().second; refresh++)
                 MEMORY[refresh] = 0;
@@ -133,13 +139,16 @@ void updMemory(clock_t time){
     
 }
 
+// функция, которая позволяет понять, попадет ли процесс
+// в память или отправится в своп-файл
 int goInMemory(Process* exe){
-    //смотрим, сколько нужно памяти для процесса
+    // смотрим, сколько нужно памяти для процесса
     int memory = exe->getMemory();
     int begin;
     for (int i = 0; i < CAPACITY; i++){
         int noSpace = 0;
         for (int j = i; j < i + memory; j++)
+        // память
             if (MEMORY[j] == 1){
                 noSpace = 1;
                 break;
@@ -154,28 +163,33 @@ int goInMemory(Process* exe){
     return 0;
 }
 
+// обновление списка процессов
 void updProcesses(){
+    // сортируем процессы в порядке возрастания приоритета выполнения
     sort(query.begin(), query.end());
     for (int i = 0; i < query.size(); i++){
+        // если процесс только создали и не находится ни в памяти, не в своп-файле
         if (query[i].getSwapping() == -1){
-            //пишем в память этот процесс
-            int prior = query[i].getPriority();
             //cout << "1";
+            // смотрим, хватит ли в данный момент процессу столько памяти, сколько он просит
             int successfullyEntered = goInMemory(&query[i]);
             //cout << "POS: " << query[i].getInMemory().first << " - " << query[i].getInMemory().second << endl;
-            //меняем параметр своппинга
+            // если хватает, то загружаем память и помещаем процесс в исполнение
             if (successfullyEntered){
                 for (int j = query[i].getInMemory().first; j <= query[i].getInMemory().second; j++)
                     MEMORY[j] = 1;
                 query[i].setSwapping(0);
             }
+            // иначе заносим процесс в своп-файл
             else 
                 query[i].setSwapping(1);
         }
+        // если процесс находится в своп-файле, то пробуем переместить в память
         else if (query[i].getSwapping() == 1){
+            // смотрим, хватит ли в данный момент процессу столько памяти, сколько он просит
             int successfullyEntered = goInMemory(&query[i]);
             //cout << "POS: " << query[i].getInMemory().first << " - " << query[i].getInMemory().second << endl;
-            //меняем параметр своппинга
+            // если хватает, то загружаем память и помещаем процесс в исполнение
             if (successfullyEntered){
                 for (int j = query[i].getInMemory().first; j <= query[i].getInMemory().second; j++)
                     MEMORY[j] = 1;
@@ -185,6 +199,7 @@ void updProcesses(){
     } 
 }
 
+// обновление графического мониторинга оперативной памяти
 bool updStatusMonitor(clock_t timeProgram){
 
     //для работы с цветом в консоли
@@ -198,17 +213,20 @@ bool updStatusMonitor(clock_t timeProgram){
     cout << "Press key for update...\n";
     while (exit != 'q'){
         myTime = clock() / CLOCKS_PER_SEC;
-
+        // каждую пройденную секунду производим обновление параметров
         if (myTime - oldTime  >= 1){
             //test memory slots
             //MEMORY[rand() % 1000] = rand() % 2;
             //updProcesses();
+
             system("cls");
             updMemory(myTime);
             updProcesses();
             
             cout << "MONITOR STATUS\t\tWORKING TIME PROGRAMM: " << myTime << "sec\n";
-            cout << "\n\nRandom Access Memory:\n";
+            cout << "\n\nCAPACITY OF MEMORY - " << CAPACITY << " byte";
+            // отрисовка оперативной памяти
+            cout << "\nRandom Access Memory:\n";
             cout << " ";
             for (int i = 0; i < 100; i++)
                 cout << "-";
@@ -234,7 +252,7 @@ bool updStatusMonitor(clock_t timeProgram){
             for (int i = 0; i < 100; i++)
                 cout << "-";
             cout << " \n";
-
+            // отрисовка всех процессов по приоритету
             cout << "\n\nCurrent opening processes:\n";
             for (int i = 0; i < query.size(); i++){
                 cout << endl << i + 1 << ". " << query[i].getName() << " (Priority " << query[i].getPriority() << ") - ";
@@ -256,6 +274,7 @@ bool updStatusMonitor(clock_t timeProgram){
     return 1;
 }
 
+// основная программа - небольшое лист меню
 int main(){
     srand(time(NULL));//для тестирование памяти
     clock_t myTime = clock() / CLOCKS_PER_SEC, oldTime = myTime;
